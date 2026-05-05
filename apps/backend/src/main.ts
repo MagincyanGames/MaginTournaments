5df6f6c51f824597d6ec55902aa6874699ed23d6
CarlosPalmaSantos <carlospalmansantos4@gmail.com>
2026-05-05 16:43:50 +0200

    feat: add swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('MaginTournament API')
+    .setDescription('')
+    .setVersion('0.1')
+    .build()
+
+  const documentFactory = ()=>SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+,
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
 }
