8086a3fee7544a53424e637e3ce801ba289919d3
CarlosPalmaSantos <carlospalmansantos4@gmail.com>
2026-05-05 17:08:04 +0200

    feat: typeorm connection

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WSGateway } from './websocket/ws.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'better-sqlite3',
+    database: 'db.sqlite',
+    entities: [__dirname+'/**/*.entity{.ts,.js}'],
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService, WSGateway],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   const documentFactory = ()=>SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, documentFactory)
-,
+
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
 }
